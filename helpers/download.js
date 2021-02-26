2e315ff23202c43ff29e41c5c682fb5b0d825561
Alexander Velichko <alvi@mail.ua>
2021-02-26 22:08:47 +0200

    added folder

--- helpers/download.js
+++ helpers/download.js
@@ -3,16 +3,16 @@ import fs from 'fs';
 import {nanoid} from 'nanoid'
 
 export default (url) => {
-    return new Promise((resolve, reject) => {
-        const fileName = `${nanoid()}.webp`
-        let file = fs.createWriteStream(`./files/${fileName}`);
-        https.get(url, function (response) {
-            response.on('data', function (chunk) {
-                file.write(chunk)
-            })
-            response.on('end', function () {
-                resolve(fileName)
-            })
-        })
+  return new Promise((resolve, reject) => {
+    const fileName = `${nanoid()}.webp`
+    let file = fs.createWriteStream(`./files/${fileName}`);
+    https.get(url, function (response) {
+      response.on('data', function (chunk) {
+        file.write(chunk)
+      })
+      response.on('end', function () {
+        resolve(fileName)
+      })
     })
+  })
 }
