d4a26e650b02bf5e220835f91734af0a73acf35f
Alexander Velichko <alvi@mail.ua>
2021-02-26 22:13:58 +0200

    debug

--- index.js
+++ index.js
@@ -16,40 +16,42 @@ app.get('/', (req, res) => res.render('index', {title: 'Hey', message: 'Hello th
 )
 
 app.post('/', async (req, res) => {
-    try {
-        const {youtubeId} = req.body;
-        const url = await getThumbnail(youtubeId);
-        const localUrl = await downloadFile(url);
-        console.log(localUrl)
-        res.render('result',
-            {
-                title: 'Result',
-                url: localUrl,
-            });
-    } catch (e) {
-        console.log(e)
-    }
-
-
+  try {
+    const {youtubeId} = req.body;
+    const url = await getThumbnail(youtubeId);
+    const localUrl = await downloadFile(url);
+    console.log(localUrl)
+    res.render('result',
+      {
+        title: 'Result',
+        url: localUrl,
+      });
+  } catch (e) {
+    console.log(e)
+  }
+  
+  
 })
 
 app.listen(process.env.PORT || port, () => console.log('Server is running'));
 
 async function fetchHtml(id) {
-    const url = `https://www.youtube.com/results?search_query=${id}`;
-    return await fetch(url, {
-        headers: {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
-        }
-    }).then(x => x.text());
+  const url = `https://www.youtube.com/results?search_query=${id}`;
+  return await fetch(url, {
+    headers: {
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+    }
+  }).then(x => x.text());
 }
 
 
 async function getThumbnail(id) {
-    const html = await fetchHtml(id);
-    const pattern = `(https:\/\/i\.ytimg\.com\/an_webp\/${id}/mqdefault.+?)",`
-    const regx = new RegExp(pattern, 'gi');
-    const results = html.match(regx);
-    const [result] = results;
-    return result.replace(`",`, '').replace(/\\u0026/g, '&');
+  const html = await fetchHtml(id);
+  const pattern = `(https:\/\/i\.ytimg\.com\/an_webp\/${id}/mqdefault.+?)",`
+  const regx = new RegExp(pattern, 'gi');
+  const results = html.match(regx);
+  console.log(results);
+  const [result] = results;
+  console.log(result);
+  return result.replace(`",`, '').replace(/\\u0026/g, '&');
 }
